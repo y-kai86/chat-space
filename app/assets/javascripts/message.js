13d1c2d81426dcfd3b3981b69839e46547dae77f
bdr86 <y.kai486@gmail.com>
2020-02-18 21:50:00 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -61,6 +61,9 @@ $(function(){
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+    })
+    .always(function(){
+      $('.chat-main__form-send').prop('disabled', false);
     });
   })
-});
+})
